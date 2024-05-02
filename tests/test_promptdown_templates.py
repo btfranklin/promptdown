9c40b93799b8936a79ee3532cd986e51bad6d41d
B.T. Franklin <brandon.franklin@gmail.com>
2024-05-01 20:44:38 -0500

    Update unit tests to include templating in system message.

--- tests/test_promptdown_templates.py
+++ tests/test_promptdown_templates.py
@@ -6,7 +6,7 @@ def test_apply_template_values_to_object():
     # Create a structured prompt with placeholders
     structured_prompt = StructuredPrompt(
         name="Test Prompt",
-        system_message="You are a helpful expert at geography.",
+        system_message="You are a helpful expert at {{topic}}.",
         conversation=[
             Message(role="User", content="What is the capital of {{country}}?"),
             Message(
@@ -16,12 +16,13 @@ def test_apply_template_values_to_object():
     )
 
     # Define template values to apply
-    template_values = {"country": "France", "capital": "Paris"}
+    template_values = {"country": "France", "capital": "Paris", "topic": "geography"}
 
     # Action: Apply the template values
     structured_prompt.apply_template_values(template_values)
 
     # Assertions to check if the placeholders are correctly replaced
+    assert structured_prompt.system_message == "You are a helpful expert at geography."
     assert structured_prompt.conversation[0].content == "What is the capital of France?"
     assert (
         structured_prompt.conversation[1].content == "The capital of France is Paris."
@@ -36,24 +37,25 @@ def test_apply_template_values_to_string():
 
 ## System Message
 
-You are a helpful expert at geography.
+You are a helpful expert at {{topic}}.
 
 ## Conversation
 
 | Role | Content |
 |---|---|
 | User | What is the capital of {{country}}? |
-| Assisant | The capital of {{country}} is {{capital}}. |
+| Assistant | The capital of {{country}} is {{capital}}. |
 """
     structured_prompt = StructuredPrompt.from_promptdown_string(prompt_string)
 
     # Define template values to apply
-    template_values = {"country": "France", "capital": "Paris"}
+    template_values = {"country": "France", "capital": "Paris", "topic": "geography"}
 
     # Action: Apply the template values
     structured_prompt.apply_template_values(template_values)
 
     # Assertions to check if the placeholders are correctly replaced
+    assert structured_prompt.system_message == "You are a helpful expert at geography."
     assert structured_prompt.conversation[0].content == "What is the capital of France?"
     assert (
         structured_prompt.conversation[1].content == "The capital of France is Paris."
